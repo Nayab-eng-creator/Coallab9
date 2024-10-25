49cceff15ce215e00a7900005a5c95c9d00f7ce7
Nayab-eng-creator <nayab.goher786@gmail.com>
2024-10-25 11:44:10 -0700

    Add files via upload

---
+++ Coallab9task2.asm
@@ -0,0 +1,55 @@
+.model small
+.stack 100h
+.data
+msg1 DB 10,13, "Enter your lab mid marks (0-9): $"
+msg2 DB 10,13, "Need hard work.$"
+msg3 DB 10,13, "Satisfactory.$"
+msg4 DB 10,13, "Invalid input! Please enter a digit between 0 and 9.$"
+.code
+main proc
+mov ax, @data
+mov ds, ax
+
+mov dx, offset msg1
+mov ah, 9
+int 21h
+
+mov ah, 1
+int 21h
+    sub al, 30h   
+
+
+cmp al, 9
+jginvalid_input
+cmp al, 0
+jlinvalid_input
+
+cmp al, 5
+jlhard_work
+
+
+mov dx, offset msg3
+mov ah, 9
+int 21h
+jmpend_program
+
+hard_work:
+
+mov dx, offset msg2
+mov ah, 9
+int 21h
+jmpend_program
+
+invalid_input:
+
+mov dx, offset msg4
+mov ah, 9
+int 21h
+
+end_program:
+
+mov ah, 4Ch
+int 21h
+main endp
+end main
+
